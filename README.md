9bc896fb9713c4656083c68479ed2f4b2108ec31
theunk51 <poptropican51@gmail.com>
2021-03-01 14:34:29 +0000

    added some new issue; more debugging

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
 # korean-quiz
-a git repo of the repl.it 'k'
-A flashcard system inspired by Conjugemos' graded practice, Quizlet, and Anki.
+A git repo of the repl.it `k` which is a supposed flashcard system inspired by Conjugemos' graded practice, Quizlet, and Anki.
 
-### Goals
-- [ ] make this project a flask app
+## Goals
+- [ ] make this project a flask app or a website
 - [ ] intergrate a recommendation algorithm to suggest words that need review, testing, learning, or mastery
 - [ ] automate word updates from a google sheet
 
-### To Do
+## To Do
 - [ ] reogranize code into OOP structure
-- [ ] simplifeid functions
+- [ ] simplified functions
 - [ ] add a grahpical interface
 - [ ] add a `learn`, `review`, `test` and `master` function
 - [ ] add color to the score, correct, incorrect and terms
 - [ ] add more vocab
 - [ ] add grammar
 - [ ] add reversed term and definiton practices
+
 ## Current Issues
-
-
+- a possible regex implemention may be need to check answers 
 
 
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import random, os
 
 with open("word.txt") as f:
-  dictionary = {line.split(' - ')[1]: line.replace('\n','').split(' - ')[0] for line in f}
+  dictionary = {line.replace('\n', '').split(' - ')[1]: line.replace('\n','').split(' - ')[0] for line in f}
   f.close()
 
 
-
-wordcount = 1
+wordcount = 2
 wordList, userDict = {}, {}
 
 for x in range(wordcount):
@@ -17,31 +16,32 @@ word = list(wordList.keys())
 
 while len(word) > 0:
   term = word.pop()
-  userDict[term] = input(f'\n{term}>>> ')
+  userDict[term] = input(f'\n{term}\n >>> ')
   
   if len(word) == 0:
     print('\n\nDone!')
 
 
 
-def check(function):
-	def score(incorrect, correct):
-		for key, val in wordList.items():
-			for term, answer in userDict.items():
-				if key ==term and val == answer:
-					correct+= 1
-				elif key == term and answer != val:
-					incorrect.append([key, val, answer])
-		function([], 0)
-	return score
-	
 
-@check
-def results():
+def score(incorrect, correct):
+  for key, val in wordList.items():
+    for term, answer in userDict.items():
+      if key ==term and val == answer:
+        correct+= 1
+      elif key == term and answer != val:
+        incorrect.append([key, val, answer])
+
+  return incorrect, correct
+	
+res = score([],0)
+incorrect, correct = res[0], res[1]
+
+def results(incorrect, correct):
   os.system('clear')
   print(f'Your score is: {correct}/{wordcount}\n\n\n')
   for i in incorrect: 
-    print(f'{i[0]}: {i[1]}\nYour answer: {i[2]}')
-	
-	
-		
+    print(f'{i[0]}: {i[1]}\nYour answer: {i[2]}\n')
+
+results(incorrect, correct)
+
